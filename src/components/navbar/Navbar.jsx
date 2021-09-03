dfec30381fb1f3add43bf2eea5b0f95b53c5f94d
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-09-03 03:28:19 +0200

    burger menu pour mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,11 +6,19 @@ export default class Navbar extends Component {
         return(
             <header className="navbar navbar-expand-lg navbar-dark" >
                 <a className="navbar-brand" href="/">Yoan Deconinck</a>
-                <button className="navbar-toggler">
+                <button 
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#toggleMobileMenu"
+                    aria-controls="toggleMobileMenu"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
+                <div className="collapse navbar-collapse" id="toggleMobileMenu">
+                    <ul className="navbar-nav ml-auto text-center">
                         <li className="nav-item ml-2">
                             <NavLink to='/' className="nav-link">Home</NavLink>
                         </li>
