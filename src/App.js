db0b2cd643a08ad6b465ce05ebc484dd33af22b3
Yoan15 <72350941+Yoan15@users.noreply.github.com>
2021-09-03 04:11:46 +0200

    ajout composant social + ajout composant fixe (profile) et composant changeant sur la même page + background qui prend toute la page (cependant bug ligne blanche côté droit et parfois gauche sur desktop et entre footer et toute la page sur mobile)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home';
+import Profile from './components/profile/Profile';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Social from './components/social/Social';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
+        <div className="row">
+        <div className="col-lg-6 col-md-12">
+          <Switch>
+            <Route 
+              exact path="/"
+              component={Home}
+            />
+
+            <Route 
+              path="/social"
+              component={Social}
+            />
+          </Switch>
+        </div>
+        <div className="col-lg-6 col-md-12">
+          <Profile/>
+        </div>
+      </div>
       </BrowserRouter>
-        <Home/>
-        <Footer/>
+      
+      <Footer/>
     </div>
   );
 }
